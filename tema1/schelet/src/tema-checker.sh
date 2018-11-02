0627cba48dc0146cac1399ff69513d1471061d53
Abi2737 <4ndreiene@gmail.com>
2018-11-02 12:36:36 +0200

    Tema1 2018 (#12)
    * Added tests v3
    
    * Added scheleton of code.
    
    * moved checker in rigth folder
    
    * Added tests
    
    * Update test4.in.ref
    
    * Update test6.in.ref
    
    * Update test7.in.ref
    
    * bonus tests

--- tema1/schelet/src/tema-checker.sh
+++ tema1/schelet/src/tema-checker.sh
@@ -6,12 +6,14 @@ RESOURCES_DIRECTORY="$CURRENT_DIRECTORY/checker/tests"
 GOOD_TESTS=0
 GOOD_CODE_STYLE=`echo -ne "Starting audit...\nAudit done.\n"`
 BAD_CODE_STYLE=0
+BONUS_TESTS_SCORE=0
 
 ## FUNCTIONS ##
 function cleanHomework
 {
 	find . -name "*.class" -type f -delete
 	rm -rf "$RESOURCES_DIRECTORY/out"
+	rm -rf "$RESOURCES_DIRECTORY/bonus/out"
 }
 
 function compileHomework
@@ -19,6 +21,7 @@ function compileHomework
 	javac -g main/Main.java
 
 	mkdir "$RESOURCES_DIRECTORY/out"
+	mkdir "$RESOURCES_DIRECTORY/bonus/out"
 }
 
 function checkTest
@@ -44,7 +47,7 @@ function checkTest
 
 function checkCheckstyle
 {
-	echo -ne "Checkstyle\t\t.....................................\t"
+	echo -ne "\nCheckstyle.....................................\t"
 	java -jar checker/checkstyle/checkstyle-7.3-all.jar -c checker/checkstyle/poo_checks.xml *  > checkstyle.txt
 
 	YOUR_CODE_STYLE=`cat checkstyle.txt`
@@ -64,6 +67,94 @@ function checkCheckstyle
 	fi
 }
 
+function checkBonus
+{
+    echo -ne "Test\t$1\n"
+    CRT_BONUS_FILE="$RESOURCES_DIRECTORY/bonus/input/$1.in"
+    PLAYER=$(sed -n 2p $CRT_BONUS_FILE)
+    WIZARD="wizard"
+    ORDER="['$WIZARD', '$PLAYER']"
+
+    BONUS_TEST="$RESOURCES_DIRECTORY/bonus/input/bonusTest.in"
+    touch $BONUS_TEST
+    sed -n 1p $CRT_BONUS_FILE > $BONUS_TEST
+    echo $ORDER >> $BONUS_TEST
+
+    OUT_TEST="$RESOURCES_DIRECTORY/bonus/out/$1.out"
+
+    TEST_RESULT="FAIL"
+
+    echo -ne "\t\t$WIZARD vs $PLAYER:\n"
+    java main.Main $BONUS_TEST > $OUT_TEST
+
+	if [ $? -eq 0 ]; then
+
+        WIZARD_OUT=$(echo $WIZARD | tr a-z A-Z)
+        PLAYER_OUT=$(echo $PLAYER | tr a-z A-Z)
+
+	    echo -en "\t\t\t"
+	    sed -n 1p $OUT_TEST
+
+	    echo -en "\t\t\t"
+	    sed -n 2p $OUT_TEST
+
+        WIZARD_SCORE=$(cat $OUT_TEST | grep $WIZARD_OUT | grep -o '[0-9]*')
+#        WIZARD_SCORE="137"
+        PLAYER_SCORE=$(cat $OUT_TEST | grep PLAYER_OUT | grep -o '[0-9]*')
+#        PLAYER_SCORE="109"
+
+        ROUND1_SCORE=$(($WIZARD_SCORE-PLAYER_SCORE))
+        echo -en "\t\t\t\tRound score: $ROUND1_SCORE\n"
+
+#        ROUND 2
+        rm -rf $BONUS_TEST
+
+        ORDER="['$PLAYER', '$WIZARD']"
+        touch $BONUS_TEST
+        sed -n 1p $CRT_BONUS_FILE > $BONUS_TEST
+        echo $ORDER >> $BONUS_TEST
+
+        echo -ne "\t\t$PLAYER vs $WIZARD:\n"
+        java main.Main $BONUS_TEST > $OUT_TEST
+
+        if [ $? -eq 0 ]; then
+
+            echo -en "\t\t\t"
+            sed -n 1p $OUT_TEST
+
+            echo -en "\t\t\t"
+            sed -n 2p $OUT_TEST
+
+            WIZARD_SCORE=$(cat $OUT_TEST | grep $WIZARD_OUT | grep -o '[0-9]*')
+#            WIZARD_SCORE="128"
+            PLAYER_SCORE=$(cat $OUT_TEST | grep PLAYER_OUT | grep -o '[0-9]*')
+#            PLAYER_SCORE="109"
+
+            ROUND2_SCORE=$(($WIZARD_SCORE-PLAYER_SCORE))
+            echo -en "\t\t\t\tRound score: $ROUND2_SCORE\n"
+
+            TOTAL_SCORE=$(($ROUND1_SCORE+$ROUND2_SCORE))
+            if [ $TOTAL_SCORE -gt 0 ]; then
+                BONUS_TESTS_SCORE=$((BONUS_TESTS_SCORE+1))
+
+                TEST_RESULT="OK (total score: $TOTAL_SCORE)"
+            else
+                TEST_RESULT="FAIL (total score: $TOTAL_SCORE)"
+            fi
+
+            echo -ne "\n\t$1 $TEST_RESULT\n"
+        else
+            echo -ne '\t\tFAIL (program error)\n'
+        fi
+    else
+        echo -ne '\t\tFAIL (program error)\n'
+    fi
+
+    rm -rf $BONUS_TEST
+
+    echo -ne "................................................\n"
+}
+
 function calculateScore
 {
 	GOOD_TESTS=$((60-GOOD_TESTS))
@@ -72,7 +163,8 @@ function calculateScore
 	BAD_CODE_STYLE=`echo "scale=2; $BAD_CODE_STYLE" | bc -l`
 
 	echo -ne "\n-$GOOD_TESTS failed tests"
-	echo -ne "\n-$BAD_CODE_STYLE checkstyle errors\n\n"
+	echo -ne "\n-$BAD_CODE_STYLE checkstyle errors"
+	echo -ne "\n+$BONUS_TESTS_SCORE bonus tests\n\n"
 }
 
 ## MAIN EXECUTION ##
@@ -89,6 +181,28 @@ checkTest "test6"
 checkTest "test7"
 checkTest "test8"
 checkTest "test9"
+
+checkBonus "bonus1"
+checkBonus "bonus2"
+checkBonus "bonus3"
+checkBonus "bonus4"
+checkBonus "bonus5"
+checkBonus "bonus6"
+checkBonus "bonus7"
+checkBonus "bonus8"
+checkBonus "bonus9"
+checkBonus "bonus10"
+checkBonus "bonus11"
+checkBonus "bonus12"
+checkBonus "bonus13"
+checkBonus "bonus14"
+checkBonus "bonus15"
+checkBonus "bonus16"
+checkBonus "bonus17"
+checkBonus "bonus18"
+checkBonus "bonus19"
+checkBonus "bonus20"
+
 checkCheckstyle
 
 calculateScore
