0d5e2c0e9fa2e5f53c65f084dd0ef6b21483727a
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-12-01 19:56:59 +0200

    Move tema1 to the correct folder (#26)

---
+++ teme/tema1/skel/src/com/tema1/common/Constants.java
@@ -0,0 +1,5 @@
+package com.tema1.helpers;
+
+public final class Constants {
+	// add/delete any constants you think you may use
+}
---
+++ teme/tema1/skel/src/com/tema1/main/GameInputLoader.java
@@ -0,0 +1,47 @@
+package com.tema1.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fileio.FileSystem;
+
+public class GameInputLoader {
+	// DO NOT MODIFY
+	private final String mInputPath;
+	private final String mOutputPath;
+
+    GameInputLoader(final String inputPath, final String outputPath) {
+        mInputPath = inputPath;
+        mOutputPath = outputPath;
+    }
+
+    public GameInput load() {
+    	List<Integer> assetsIds = new ArrayList<>();
+        List<String> playerOrder = new ArrayList<>();
+        int rounds = 0;
+        int noPlayers = 0;
+        int noGoods = 0;
+        
+    	try {
+			FileSystem fs = new FileSystem(mInputPath, mOutputPath);
+
+			rounds = fs.nextInt();
+			noPlayers = fs.nextInt();
+			
+			for(int i = 0; i < noPlayers; ++i)
+				playerOrder.add(fs.nextWord());
+			
+			noGoods = fs.nextInt();
+		
+			for(int i = 0; i < noGoods; ++i)
+				assetsIds.add(fs.nextInt());
+			
+			fs.close();
+			
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+        return new GameInput(rounds, assetsIds, playerOrder);
+    }
+}
