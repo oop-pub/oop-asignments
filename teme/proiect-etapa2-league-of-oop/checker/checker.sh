38fd1468be711075286c8ff478e0702dc216e2f0
Laurentiu Stamate <laurentiu@Laurentius-MacBook-Pro.local>
2019-12-24 18:02:23 +0200

    Improve checker with forum comments

--- teme/proiect-etapa2-league-of-oop/checker/checker.sh
+++ teme/proiect-etapa2-league-of-oop/checker/checker.sh
@@ -32,7 +32,7 @@ function checkTest
     java main.Main "$RESOURCES_DIRECTORY/in/$1.in" "$RESOURCES_DIRECTORY/out/$1.out" > /dev/null
 
 	if [ $? -eq 0 ]; then
-        `diff -Bw -u --ignore-all-space $RESOURCES_DIRECTORY/out/$1.out $RESOURCES_DIRECTORY/res/$1.in.res &> /dev/null`
+        `diff -Bw -u --ignore-all-space "$RESOURCES_DIRECTORY/out/$1.out" "$RESOURCES_DIRECTORY/res/$1.in.res" &> /dev/null`
         DIFF_RESULT=$?
 
         if [ $DIFF_RESULT -eq 0 ]; then
